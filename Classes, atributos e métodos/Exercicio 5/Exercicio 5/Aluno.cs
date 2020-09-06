a625d3322458a007b30449aa1b076a35628af3d3
luccaki <49926738+luccaki@users.noreply.github.com>
2020-09-06 19:39:20 -0300

    Add files via upload

---
+++ Classes, atributos e métodos/Exercicio 5/Exercicio 5/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_5 {
+    class Aluno {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal() {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public override string ToString() {
+            if (NotaFinal() >= 60) {
+                return "NOTA FINAL: " + NotaFinal() + "\nAprovado";
+            }
+            else {
+                return "NOTA FINAL: " + NotaFinal() + "\nReprovado\nFaltaram " + (60 - NotaFinal()) + " Pontos"; 
+            }
+        }
+    }
+}
---
+++ Classes, atributos e métodos/Exercicio 5/Exercicio 5/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Fazer um programa para ler o nome de um aluno e as 3 primeiras notas dele(primeira vale 30 e segunda e terceira vale 35)
+Ao final, mostrar qual a nota final do aluno
+Dizer tambem se o aluno foi aprovado ou não, e em caso negativo, quantos pontos faltam para o launo obter o minimo para ser aprovado(60 pontos)
+*/
+
+using System;
+using System.Globalization;
+
+namespace Exercicio_5 {
+    class Program {
+        static void Main(string[] args) {
+
+            Aluno x = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            x.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as 3 notas do aluno: ");
+            x.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(x);
+        }
+    }
+}
