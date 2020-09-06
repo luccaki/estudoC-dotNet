f90ba553cb81fcf33514aeadd84df0a49ff36628
luccaki <49926738+luccaki@users.noreply.github.com>
2020-09-06 20:17:30 -0300

    Add files via upload

---
+++ Membros Estaticos/Membros Estaticos/ConversorDeMoeda.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Membros_Estaticos {
+    class ConversorDeMoeda {
+        public static double Dolar(double cot, double dol) {
+            return cot * dol * 1.06;
+        }
+    }
+}
---
+++ Membros Estaticos/Membros Estaticos/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Faça um programa para ler a cotação do dollar, e depois um valor em dolares a ser comprado por uma pessoas em reais
+ * Informar quantos reias a pessoa vai pagar pelo dolares, considerando ainda a taxa de 6% de IOF
+ */
+
+using System;
+using System.Globalization;
+
+namespace Membros_Estaticos {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Qual é a cotação do dolar?: ");
+            double cot = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolares você vai comprar?: ");
+            double dol = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor a ser pago em rais: "+ ConversorDeMoeda.Dolar(cot,dol).ToString("F2",CultureInfo.InvariantCulture));]
+        }
+    }
+}
