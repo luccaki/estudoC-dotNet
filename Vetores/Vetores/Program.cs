54991dff95b1128221d9fb21df5c679c6371b8dd
luccaki <49926738+luccaki@users.noreply.github.com>
2020-09-07 04:41:09 -0300

    Add files via upload

---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores {
+    class Program {
+        static void Main(string[] args) {
+            Quarto[] quartos = new Quarto[10];
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++) {
+                Console.Write("Alguel #" + (i+1) + ":\nNome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                quartos[quarto] = new Quarto { Nome = nome, Email = email };
+                Console.WriteLine();
+            }
+            Console.WriteLine("Quaros ocupados: ");
+            for (int i = 0; i < 10; i++) {
+                if (quartos[i] != null) {
+                    Console.Write(i + ": ");
+                    Console.Write(quartos[i] + "\n");
+                }
+            }
+        }
+    }
+}
---
+++ Vetores/Vetores/Quarto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetores {
+    class Quarto {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString() {
+            return Nome + ", " + Email;
+        }
+    }
+}
