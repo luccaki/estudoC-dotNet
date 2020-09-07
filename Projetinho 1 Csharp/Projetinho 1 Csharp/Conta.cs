2a9c4862e14ef0299c9875692928cfce15650276
luccaki <49926738+luccaki@users.noreply.github.com>
2020-09-06 22:04:45 -0300

    Add files via upload

---
+++ Projetinho 1 Csharp/Projetinho 1 Csharp/Conta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Projetinho_1_Csharp {
+    class Conta {
+        public int Numero { get; private set; }
+        public string Nome { get; set; }
+        public double Dinheiro { get; private set; }
+
+        public Conta() {
+        }
+        public Conta(int numero, string nome) {
+            Nome = nome;
+            Numero = numero;
+        }
+
+        public Conta(int numero, string nome, double dinheiro) : this(numero, nome) {
+            Dinheiro = dinheiro;
+        }
+
+        public void Deposito(double quantidade) {
+            Dinheiro += quantidade;
+        }
+
+        public void Saque(double quantidade) {
+            Dinheiro -= quantidade + 5;
+        }
+
+        public override string ToString() {
+            return "Dados da conta atualizados:\nConta " + Numero + ", Titular: " + Nome + ", Saldo: $ " +
+                Dinheiro.ToString("F2", CultureInfo.InvariantCulture) + "\n";
+        }
+    }
+
+}
---
+++ Projetinho 1 Csharp/Projetinho 1 Csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projetinho_1_Csharp {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Entre o numero da conta:");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta:");
+            string nome = Console.ReadLine();
+
+            Conta conta = new Conta(num, nome);
+
+            Console.Write("Haverá depósito inicial(s/n) ?");
+            if (Console.ReadLine() == "s") {
+                Console.Write("Entre o valor de depósito inicial: ");
+                conta.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            }
+
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para deposito: ");
+            conta.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para saque: ");
+            conta.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(conta);
+        }
+    }
+}
