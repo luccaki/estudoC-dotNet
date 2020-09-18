60117f97812edd19cad87d2a7d397808cbc58223
luccaki <49926738+luccaki@users.noreply.github.com>
2020-09-18 01:45:22 -0300

    Add files via upload

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ Na contagem de votos de uma eleição, são gerados vários registros
+de votação contendo o nome do candidato e a quantidade de votos
+(formato .csv) que ele obteve em uma urna de votação. Você deve
+fazer um programa para ler os registros de votação a partir de um
+arquivo, e daí gerar um relatório consolidado com os totais de cada
+candidato.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> candidatos = new Dictionary<string, int>();
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        if (candidatos.ContainsKey(line[0]))
+                        {
+                            candidatos[line[0]] += int.Parse(line[1]);
+                        }
+                        else
+                        {
+                            candidatos.Add(line[0], int.Parse(line[1]));
+                        }
+                    }
+                    foreach(var x in candidatos)
+                    {
+                        Console.WriteLine(x.Key + ": " + x.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
