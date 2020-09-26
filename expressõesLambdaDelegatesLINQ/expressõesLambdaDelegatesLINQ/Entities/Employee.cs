8436db661951a8d4b0733682391746428df5f449
luccaki <49926738+luccaki@users.noreply.github.com>
2020-09-26 03:43:43 -0300

    Add files via upload

---
+++ expressõesLambdaDelegatesLINQ/expressõesLambdaDelegatesLINQ/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expressõesLambdaDelegatesLINQ.Entities
+{
+    class Employee
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public double Salary { get; private set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
---
+++ expressõesLambdaDelegatesLINQ/expressõesLambdaDelegatesLINQ/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Fazer um programa para ler os dados (nome, email e salário)
+de funcionários a partir de um arquivo em formato .csv.
+Em seguida mostrar, em ordem alfabética, o email dos
+funcionários cujo salário seja superior a um dado valor
+fornecido pelo usuário.
+Mostrar também a soma dos salários dos funcionários cujo
+nome começa com a letra 'M'.
+*/
+
+using System;
+using expressõesLambdaDelegatesLINQ.Entities;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace expressõesLambdaDelegatesLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> employees = new List<Employee>();
+
+            using(StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    string name = line[0];
+                    string email = line[1];
+                    double sal = double.Parse(line[2], CultureInfo.InvariantCulture);
+                    employees.Add(new Employee(name, email, sal));
+                }
+            }
+
+            Console.Write("Enter salary: ");
+            double salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Email of people whose salary is more than " + salary.ToString("F2", CultureInfo.InvariantCulture) + " :");
+
+            var output = employees.Where(p => p.Salary > salary).OrderBy(p => p.Email).Select(p => p.Email);
+            foreach(string o in output)
+            {
+                Console.WriteLine(o);
+            }
+
+            Console.Write("Sum of salary of people whose name starts with 'M': ");
+            var sum = employees.Where(p => p.Name.IndexOf('M')==0 ).Select(p => p.Salary).Sum();
+            Console.WriteLine(sum.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
